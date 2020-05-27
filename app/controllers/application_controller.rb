6d11f42abd9a42dbe0836bcaa501c2b893f3643c
Lanrad09 <ola@DESKTOP-8P0EEHJ.localdomain>
2020-05-27 18:41:14 +0200

    added validation to post controller and application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+    # making sure we can still sign in and out before action 
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected 
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
 end
