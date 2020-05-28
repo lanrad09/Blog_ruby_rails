e15b2bb73099eb6d4a38d9d44d4be74739ba977b
Lanrad09 <ola@DESKTOP-8P0EEHJ.localdomain>
2020-05-28 14:39:43 +0200

    added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # The root of the site routed with "root"
+  root 'posts#index'
 end
